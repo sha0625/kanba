eb82de045309a7ed3f1408cb498b0a437304e0e0
莎莎 <L2773742953@126.com>
2019-12-15 11:49:51 +0800

    增加 components

---
+++ src/components/Header/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <header id="header">
+    <h1>{{title}}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: " "
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+#header {
+  width: 100%;
+  height: 50px;
+  color: #fff;
+  background: #248067;
+  border-bottom: 1px solid #248067;
+  position: relative;
+}
+#header h1 {
+  font-size: 18px;
+  text-align: center;
+  line-height: 50px;
+  font-weight: normal;
+}
+#header i {
+  position: absolute;
+  left: 5px;
+  top: 50%;
+  margin-top: -13px;
+  font-size: 26px;
+}
+</style>
